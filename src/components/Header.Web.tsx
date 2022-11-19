60db8b2b67846e52fbaf1bb4f93c02432e6cd3b5
G4br1elM4t0s <gabrielmatos010203@gmail.com>
2022-11-19 08:15:47 -0300

    HeaderWeb concluido

--- src/components/Header.Web.tsx
+++ src/components/Header.Web.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 export interface HeaderProps {}
 
+
+
 const Itens = styled.a`
   font-size: 14px;
   color: #171520;
@@ -14,19 +16,16 @@ const Header = styled.header`
   display: flex;
   width: 100%;
   height: 80px;
+  background:#FFF;
   align-items: center;
+  font-family:'Inter', sans-serif;
 `;
 
 const Container = styled.div`
   display: flex;
 `;
 
-interface LogoProps {
-  src: string;
-  alt: string;
-}
-
-const Logo = styled.img<LogoProps>`
+const Logo = styled.img`
   width: 108px;
   height: 22px;
   margin-right: 33px;
@@ -49,21 +48,55 @@ const Input = styled.input`
   type: "text";
   font-size: 14px;
   border: none;
+  outline: none;
+
   background: "transparent";
 `;
 
-const Lupa = styled.img<LogoProps>`
+const Lupa = styled.img`
   width: 24px;
   height: 24px;
   margin-left:16px;
   margin-right:12px;
 `;
 
-import lupa from "./lupa.svg";
+const ContainerUtils = styled.div`
+  display: flex;
+  margin-left:24px;
+  align-items:center;
+  margin-right:18px;
+`;
+
+const Bag = styled.img`
+  width: 24px;
+  height: 24px;
+  cursor:pointer;
+
+`
+
+const Perfil = styled.img`
+  width: 18px;
+  height: 17px;
+  margin-right:23px;
+  cursor:pointer;
+`
+
+const Curtida = styled.img`
+  width: 19px;
+  height: 17px;
+  margin-right:23px;
+  cursor:pointer;
+`
+
+import lupa from "./img/lupa.svg";
+import bolsa from "./img/icon/bag.svg";
+import perfil from "./img/icon/profile.svg";
+import curtida from "./img/icon/wishlist.svg";
+
 
 // <input placeholder="Search for products or brands....." type="text" />
 
-import logo from "./logo.svg";
+import logo from "./img/logo.svg";
 
 export function HeaderWeb() {
   return (
@@ -80,7 +113,11 @@ export function HeaderWeb() {
         <Lupa src={lupa} alt="lupa" />
         <Input placeholder="Search for products or brands....."/>
       </InputBox>
-      
+      <ContainerUtils>
+      <Curtida src={curtida} alt="curtida"></Curtida>
+      <Perfil src={perfil} alt="perfil"></Perfil>
+      <Bag src={bolsa} alt="bolsa"></Bag>
+      </ContainerUtils>
     </Header>
   );
 }
