8e58a41cb588df44761195cbdd6cd253160bc1d4
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-11-25 08:39:34 -0300

    implementação da filterbarhorinzontal

--- src/screen/Category/Category.tsx
+++ src/screen/Category/Category.tsx
@@ -17,24 +17,36 @@ const SectionContent = styled.section`
   align-items: center;
   width: 100%;
   height: 100%;
-  margin-bottom:30px;
+  margin-bottom: 30px;
 `;
 
 const FilterBar = styled.div`
-display:flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
   width: 17%;
   align-self: start;
   height: 100%;
   margin-right: 13px;
 `;
 
+const ContentBox = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    height: 100%;
+    justify-content: space-around;
+    background:red;
+`
+
 const Content = styled.div`
-  width: 80%;
-  display: flex;
-  flex-wrap: wrap;
-  height: 100%;
-  justify-content: space-around;
+    width: 80%;
+    display: flex;
+    flex-wrap: wrap;
+    height: 100%;
+    justify-content: space-around;
+    background:purple;
+  
+ 
 `;
 
 const Card = styled.div`
@@ -60,24 +72,47 @@ const Price = styled.div`
   display: flex;
 `;
 
-const ChecksBox = styled.div`
-  
-`
+const ChecksBox = styled.div``;
 
 const CheckContent = styled.div`
-  display:flex;
-  margin-bottom:10px;
-`
+  display: flex;
+  margin-bottom: 10px;
+`;
 
 const Tags = styled.div`
-  display:flex;
+  display: flex;
   border-bottom: 1px solid gray;
-  margin-bottom:15px;
-`
+  margin-bottom: 15px;
+`;
 
+const FilterBarH = styled.div`
+  display: flex;
+  width:100%;
+  margin-left:40px;
+
+`;
+
+const ContainerH = styled.div`
+  display:flex;
+
+  div{
+    width:24px;
+    height:24px;
+    background:#F1F1F1;
+    text-align:center;
+
+    p{
+      color:#626262;
+
+    }
+  }
+`
 
 import { FooterWeb } from "../../components/Footer.Web";
 import bolsa from "./image.png";
+
+import grid from "./icon/icon/grid.png";
+import sort from "./icon/icon/sort.png";
 import like from "../../components/img/coracao.png";
 import estrela from "./icon/Vector-1.png";
 import estrela2 from "./icon/Vector-4.png";
@@ -86,7 +121,6 @@ import minus from "../../components/img/small-minus.png";
 export function Category() {
   return (
     <>
-      
       <HeaderWeb />
       <BannerPrincipal></BannerPrincipal>
       <SectionContent>
@@ -102,7 +136,7 @@ export function Category() {
             <img src={minus} alt="plus" />
           </Tags>
           <ChecksBox>
-          <CheckContent>
+            <CheckContent>
               <input type="checkbox" />
               <p>Blue</p>
             </CheckContent>
@@ -135,28 +169,42 @@ export function Category() {
               <p>Blue</p>
             </CheckContent>
             <Tags>
-            {" "}
-            <p>Size</p>
-            <img src={plus} alt="plus" />
-          </Tags>
-          <Tags>
-            {" "}
-            <p>Size</p>
-            <img src={plus} alt="plus" />
-          </Tags>
-          <Tags>
-            {" "}
-            <p>Size</p>
-            <img src={plus} alt="plus" />
-          </Tags>
-          <Tags>
-            {" "}
-            <p>Size</p>
-            <img src={plus} alt="plus" />
-          </Tags>
+              {" "}
+              <p>Size</p>
+              <img src={plus} alt="plus" />
+            </Tags>
+            <Tags>
+              {" "}
+              <p>Size</p>
+              <img src={plus} alt="plus" />
+            </Tags>
+            <Tags>
+              {" "}
+              <p>Size</p>
+              <img src={plus} alt="plus" />
+            </Tags>
+            <Tags>
+              {" "}
+              <p>Size</p>
+              <img src={plus} alt="plus" />
+            </Tags>
           </ChecksBox>
         </FilterBar>
         <Content>
+          <FilterBarH>
+            <div>
+              <img src={grid} alt="grid" />
+              <img src={sort} alt="sort" />
+            </div>
+            <h3>Showing 1 - 40 of 145 items</h3>
+            <ContainerH>
+              <h4>To Show</h4>
+              <div>
+                <p>9</p>
+              </div>
+            </ContainerH>
+          </FilterBarH>
+          <ContentBox>
           <Card>
             <img src={bolsa} alt="bolsa" />
             <Description>
@@ -382,10 +430,11 @@ export function Category() {
               </Price>
             </Description>
           </Card>
+          </ContentBox>
         </Content>
       </SectionContent>
 
-      <FooterWeb/>
+      <FooterWeb />
     </>
   );
 }
