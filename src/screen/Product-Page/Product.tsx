a84638283af371a280df834f0c33df052d6f1f8f
Gabriel Matos <gabrielmatos010203@gmail.com>
2022-12-14 22:21:32 -0300

    conclusão do tabs

--- src/screen/Product-Page/Product.tsx
+++ src/screen/Product-Page/Product.tsx
@@ -18,6 +18,11 @@ import {
   Discount,
   SecundaryBoxRight,
   Amount,
+  TabBar,
+  ButtonOne,
+  ButtonTwo,
+  ButtonThree,
+  TextContent,
 } from "./styleProduct";
 
 //icons e images
@@ -27,8 +32,8 @@ import { CarouselWrapper } from "../../components/Carousel";
 import estrela from "../Category/icon/Vector-1.png";
 import estrela2 from "../Category/icon/Vector-2.png";
 
-import plus from '../../components/img/plus.png';
-import menus from '../../components/img/small-minus.png'
+import plus from "../../components/img/plus.png";
+import menus from "../../components/img/small-minus.png";
 import { CouponWrapper } from "../../components/Coupon";
 
 export function Product() {
@@ -42,6 +47,12 @@ export function Product() {
   //decrease counter
   const decrease = () => {
     setCounter((count) => count - 1);
+  };
+
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index: number) => {
+    setToggleState(index);
   };
 
   return (
@@ -94,16 +105,64 @@ export function Product() {
           <Amount>
             <h3>Quantity:</h3>
             <div className="counter">
-              <img src={menus} className="control__btn_plus" onClick={decrease}/>
-              
+              <img
+                src={menus}
+                className="control__btn_plus"
+                onClick={decrease}
+              />
+
               <span className="counter__output">{counter}</span>
-              <img src={plus} className="control__btn_minus" onClick={increase}/>
-              
+              <img
+                src={plus}
+                className="control__btn_minus"
+                onClick={increase}
+              />
             </div>
           </Amount>
-          <CouponWrapper></CouponWrapper>
+          <CouponWrapper />
         </RightContent>
       </OuContent>
+      <TabBar>
+        <ButtonOne
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          Product Description
+        </ButtonOne>
+        <ButtonTwo
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          Related Products{" "}
+        </ButtonTwo>
+        <ButtonThree
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+          Ratings and Reviews{" "}
+        </ButtonThree>
+      </TabBar>
+      <TextContent>
+        <div className={toggleState === 1 ? "content active-content" : "content"}>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, facilis inventore. Aperiam pariatur dicta sit tempore quaerat incidunt nostrum eos corrupti suscipit blanditiis excepturi labore impedit officia maxime placeat cupiditate asperiores illo ab, debitis error eius harum et facere! Animi corporis dignissimos voluptas, dicta ut nostrum obcaecati beatae praesentium laboriosam.</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum deleniti placeat unde necessitatibus. Quod ducimus recusandae sed dolorem, beatae nulla.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ea!</p>
+        </div>
+        <div className={toggleState === 2 ? "content active-content" : "content"}>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, facilis inventore. Aperiam pariatur dicta sit tempore quaerat incidunt nostrum eos corrupti suscipit blanditiis excepturi labore impedit officia maxime placeat cupiditate asperiores illo ab, debitis error eius harum et facere! Animi corporis dignissimos voluptas, dicta ut nostrum obcaecati beatae praesentium laboriosam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum deleniti placeat unde necessitatibus. Quod ducimus recusandae sed dolorem, beatae nulla.</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ea!</p>
+        <p>Consectetur adipisicing elit. Provident, ea!</p>
+        </div>
+        <div className={toggleState === 3 ? "content active-content" : "content"}>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, facilis inventore. Aperiam pariatur dicta sit tempore quaerat incidunt nostrum eos corrupti suscipit blanditiis excepturi labore impedit officia maxime placeat cupiditate asperiores illo ab, debitis error eius harum et facere! Animi corporis dignissimos voluptas, dicta ut nostrum obcaecati beatae praesentium laboriosam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum deleniti placeat unde necessitatibus. Quod ducimus recusandae sed dolorem, beatae nulla.</p>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ea!</p>
+        </div>
+      </TextContent>
       <FooterWeb />
     </>
   );
--- src/screen/Product-Page/styleProduct.ts
+++ src/screen/Product-Page/styleProduct.ts
@@ -1,27 +1,23 @@
-
 import styled from "styled-components";
 
-
 export const Navigate = styled.div`
-  display: flex; 
-  margin-left:22px;
-  margin-bottom:19px;
-`
+  display: flex;
+  margin-left: 22px;
+  margin-bottom: 19px;
+`;
 
 export const ContentItem = styled.div`
   display: flex;
 
-  img{
-    width:605px;
-    height:605px;
+  img {
+    width: 605px;
+    height: 605px;
   }
-  
-`
+`;
 
 export const ContentItemBox = styled.div`
-  display:flex;
-  
-`
+  display: flex;
+`;
 
 interface ItemPros {
   urlName: string;
@@ -30,126 +26,199 @@ interface ItemPros {
 export const ContentItemPreview = styled.div<ItemPros>`
   width: 100px;
   height: 100px;
-  margin-right:31px;
+  margin-right: 31px;
   background-image: url(${(props) => props.urlName});
   background-size: contain;
   background-repeat: no-repeat;
-`
+`;
 
 export const OuContent = styled.div`
   display: flex;
   width: 100%;
-`
+`;
 export const LeftContent = styled.div`
-  margin-left:22px;
+  margin-left: 22px;
   width: 47%;
-`
+`;
 
 export const RightContent = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   align-items: flex-start;
-  margin-left:27px;
+  margin-left: 27px;
   width: 47%;
-`
+`;
 
 export const PrimaryBoxRightContent = styled.div`
   border-bottom: 1px solid rgba(0, 0, 0, 0.12);
   width: 100%;
-`
+`;
 
 export const BoxStart = styled.div`
-display: flex;
-align-items:center;
-margin-bottom:27px;
- p{
-  margin-left:16px;
-  color:#B6B6B6;
- }
-`
-
-
+  display: flex;
+  align-items: center;
+  margin-bottom: 27px;
+  p {
+    margin-left: 16px;
+    color: #b6b6b6;
+  }
+`;
 
 export const TitleAndDescription = styled.div`
-  margin-bottom:32px;
-`
+  margin-bottom: 32px;
+`;
 export const PriceBox = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom:24px;
-`
-
+  margin-bottom: 24px;
+`;
 
 export const DeliveryContent = styled.div`
-  h4{
-    font-size:20px;
+  h4 {
+    font-size: 20px;
   }
-  p{
-    font-size:16px;
-    color:#626262;
+  p {
+    font-size: 16px;
+    color: #626262;
   }
-`
+`;
 
 export const Discount = styled.div`
   width: 390px;
   height: 52px;
-  background:#F1F1F1;
-  border-radius:5px;
-  margin-left:50px;
-  input{
+  background: #f1f1f1;
+  border-radius: 5px;
+  margin-left: 50px;
+  input {
     background: transparent;
-    color:#626262;
+    color: #626262;
     width: 80%;
     height: 100%;
-    border:none;
-    font-size:16px;
-    border-radius:5px;
+    border: none;
+    font-size: 16px;
+    border-radius: 5px;
   }
-  a{
+  a {
     text-decoration: none;
-    color:#1B4B66;
-    margin-left:12px;
+    color: #1b4b66;
+    margin-left: 12px;
   }
-`
+`;
 
 export const SecundaryBoxRight = styled.div`
-  display:flex;
-  margin-top:24px;
-`
+  display: flex;
+  margin-top: 24px;
+`;
 
 export const Amount = styled.div`
-  display:flex;
-  margin-top:42px;
-  align-items:center;
-  margin-bottom:52px;
-    h3{
-      font-size:20px;
+  display: flex;
+  margin-top: 42px;
+  align-items: center;
+  margin-bottom: 52px;
+  h3 {
+    font-size: 20px;
+  }
+
+  .counter {
+    margin-left: 16px;
+    border: 1px solid #1b4b66;
+    display: flex;
+    width: 73px;
+    height: 30px;
+    border-radius: 3px;
+    align-items: center;
+    justify-content: center;
+
+    span {
+      margin-right: 7px;
+      margin-left: 7px;
+    }
+    .control__btn_plus {
+      width: 18px;
+      height: 18px;
     }
 
-    .counter{
-      margin-left:16px;
-      border: 1px solid #1B4B66;
-      display:flex;
-      width: 73px;
-      height:30px;
-      border-radius:3px;
-      align-items:center;
-      justify-content:center;
-
-    
-      span{
-        margin-right:7px;
-        margin-left:7px;
-      }
-      .control__btn_plus{
-        width: 18px;
-        height: 18px;
-      }
-
-      .control__btn_minus{
-        width: 18px;
-        height: 18px;
-      }
+    .control__btn_minus {
+      width: 18px;
+      height: 18px;
     }
-  
+  }
+`;
+
+export const TabBar = styled.div`
+  width: 98%;
+  height: 48px;
+  background: #f1f1f1;
+  display: flex;
+  margin: 0 auto;
+  border-radius: 12px;
+  align-items: center;
+
+  margin-top:63px;
+
+
+  .active-tabs {
+    & {
+      content: "";
+      display: block;
+      height: 32px;
+      color:white;
+      background: rgb(88, 147, 241);
+    }
+  }
+
+  .active-tabs {
+    border-bottom: 1px solid transparent;
+    border-radius: 8px;
+    width: 189px;
+  }
+`;
+
+export const ButtonOne = styled.button`
+  border: none;
+  margin-left:16px;
+  cursor: pointer;
+  margin-right: 24px;
+  color:#626262;
+  font-size: 16px;
+`;
+
+export const ButtonTwo = styled.button`
+  border: none;
+  cursor: pointer;
+  margin-right: 24px;
+  color:#626262;
+  font-size: 16px;
+`;
+
+export const ButtonThree = styled.button`
+  border: none;
+  cursor: pointer;
+  margin-right: 24px;
+  color:#626262;
+  font-size: 16px;
+`;
+
+
+export const TextContent = styled.div`
+  width: 80%;
+  height: 160px;
+  margin-left:1%;
+  margin-top:24px;
+  margin-bottom:161px;
+
+  .content-tabs {
+  flex-grow : 1;
+}
+.content {
+  background: white;
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  display: none;
+}
+.active-content {
+  display: block;
+}
+
 `
